8d4d0ce68e285ca63334f671cb991ebdbd466eb8
SkyyWasTaken <coolg0092002@gmail.com>
2021-12-23 22:32:51 -0500

    Bump version

--- src/main/java/com/skyywastaken/arcadelb/ArcadeLB.java
+++ src/main/java/com/skyywastaken/arcadelb/ArcadeLB.java
@@ -18,7 +18,7 @@ import java.io.File;
 public class ArcadeLB {
     public static final String NAME = "ArcadeLB";
     public static final String MODID = "arcadelb";
-    public static final String VERSION = "1.3 ALPHA";
+    public static final String VERSION = "1.4 ALPHA";
     public static Configuration configuration;
     private static Logger logger;
     public static File configDirectory;
