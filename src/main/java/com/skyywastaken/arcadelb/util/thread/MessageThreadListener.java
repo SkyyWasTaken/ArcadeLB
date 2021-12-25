a9786cdb7ce84c10d9747675eef5c39a31b3a189
SkyyWasTaken <coolg0092002@gmail.com>
2021-12-25 17:03:07 -0500

    Fixed an incompatibility issue with Patcher that would cause vanilla scoreboard text to break

--- src/main/java/com/skyywastaken/arcadelb/util/thread/MessageThreadListener.java
+++ src/main/java/com/skyywastaken/arcadelb/util/thread/MessageThreadListener.java
@@ -5,10 +5,10 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-public class ThreadListeners {
+public class MessageThreadListener {
     private final MessageHelper parentThread;
 
-    public ThreadListeners(MessageHelper parentThread) {
+    public MessageThreadListener(MessageHelper parentThread) {
         this.parentThread = parentThread;
     }
 
